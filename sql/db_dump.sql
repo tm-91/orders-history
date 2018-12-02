8e9973e793dfd938c1eab5f5b1e4c398444bdf4e
tm-91 <mateusz.tomczak@outlook.com>
2018-12-02 15:30:31 +0100

    nowy db_dump

--- sql/db_dump.sql
+++ sql/db_dump.sql
@@ -1,3 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version home.pl
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 02, 2018 at 03:26 PM
+-- Server version: 5.7.21-20-log
+-- PHP Version: 5.6.34
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `27528334_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `access_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `access_tokens` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `shop_id` int(10) unsigned DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `access_token` char(50) DEFAULT NULL,
+  `refresh_token` char(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `shop_id` (`shop_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billings`
+--
+
+CREATE TABLE IF NOT EXISTS `billings` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `shop_id` int(10) unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `shop_id` (`shop_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(11) unsigned NOT NULL,
+  `shop_order_id` int(10) NOT NULL,
+  `order_current_data` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_orders_shop_id_order_id` (`shop_id`,`shop_order_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
 -- --------------------------------------------------------
 
 --
@@ -6,42 +74,14 @@
 
 CREATE TABLE IF NOT EXISTS `orders_history` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `shop_id` int(10) unsigned DEFAULT NULL,
   `order_id` int(10) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `added` blob,
   `edited` blob,
   `removed` blob,
   PRIMARY KEY (`id`),
-  KEY `fk_orders_history` (`shop_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `orders_history`
---
-ALTER TABLE `orders_history`
-  ADD CONSTRAINT `fk_orders_history` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- --------------------------------------------------------
-
---
--- Table structure for table `orders_current_state`
---
-
-CREATE TABLE IF NOT EXISTS `orders_current_state` (
-  `shop_id` int(10) NOT NULL,
-  `order_id` int(10) NOT NULL,
-  `order_data` blob NOT NULL,
-  CONSTRAINT PRIMARY KEY
-shop_order (`shop_id`,`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `fk_orders_history_order_id` (`order_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=60 ;
 
 -- --------------------------------------------------------
 
@@ -78,3 +118,72 @@ CREATE TABLE IF NOT EXISTS `shippings` (
   `name` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shops`
+--
+
+CREATE TABLE IF NOT EXISTS `shops` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `shop` varchar(128) DEFAULT NULL,
+  `shop_url` varchar(512) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  `installed` smallint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `shop` (`shop`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscriptions`
+--
+
+CREATE TABLE IF NOT EXISTS `subscriptions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `shop_id` int(10) unsigned NOT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `shop_id` (`shop_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `access_tokens`
+--
+ALTER TABLE `access_tokens`
+  ADD CONSTRAINT `FK_access_tokens_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `billings`
+--
+ALTER TABLE `billings`
+  ADD CONSTRAINT `FK_billings_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_orders_shop_id` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders_history`
+--
+ALTER TABLE `orders_history`
+  ADD CONSTRAINT `fk_orders_history_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `subscriptions`
+--
+ALTER TABLE `subscriptions`
+  ADD CONSTRAINT `FK_subscriptions_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
