e6b2cb657976bd3678b61b664cda534e3b2ce530
tm-91 <mateusz.tomczak@outlook.com>
2018-12-10 19:42:04 +0100

    logger

--- src/DbHandler.php
+++ src/DbHandler.php
@@ -15,12 +15,11 @@ class DbHandler
                 self::$_dbHandler = new \PDO($connection['connection'], $connection['user'], $connection['pass']);
             } catch (\PDOException $e){
                 if (\Bootstraper::getConfig('debug')){
-                    \Logger::log($e->getMessage(), \Logger::TYPE_ERROR);
-                    \Logger::log($e->getTraceAsString(), \Logger::TYPE_ERROR);
+                    \Bootstraper::logger()->error($e->getMessage());
+                    \Bootstraper::logger()->error($e->getTraceAsString());
                     throw $e;
                 } else {
                     @header($_SERVER['SERVER_PROTOCOL'] . ' 500 Database connection error', true, 500);
-                    \Logger::log('Database connection error', \Logger::TYPE_ERROR);
                 }
             }
         }
