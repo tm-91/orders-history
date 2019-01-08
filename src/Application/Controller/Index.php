fcd938c87ac314fe5b020397b5ad9eb20eccd0f7
tm-91 <mateusz.tomczak@outlook.com>
2019-01-08 22:48:26 +0100

    no history fix

--- src/Application/Controller/Index.php
+++ src/Application/Controller/Index.php
@@ -6,9 +6,20 @@ class Index extends \Core\Controller\Controller
     public function indexAction()
     {
         $orderId = $this->_app->getParam('id');
-        $history = $this->_app->shop()
-            ->getOrder($orderId)
-            ->getHistory();
+        $shop = $this->_app->shop();
+        $order = false;
+        try {
+            $order = $shop->getOrder($orderId);
+        } catch (\Exception $e) {
+            // ignore
+        }
+
+        if ($order !== false) {
+            $history= $order->getHistory();
+        } else {
+            $history = [];
+        }
+
         $historyView = $this->_app->getView();
         $entryView = $this->_app->getView('Index/History/HistoryEntry');
 
